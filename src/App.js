a222c4f32d9ebeeba554fae07b72c25d0fc6e1a7
lucasmpimentel <lucas.mirandapimentel@gmail.com>
2022-01-25 18:09:11 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
-    // init
-    return (<p>Sistema Solar</p>);
+    return (
+      <main>
+        <Header />
+        <SolarSystem />
+      </main>
+    );
   }
 }
 
