a25c4b17d82456506e639d5dc536ec08c12524fb
lucasmpimentel <lucas.mirandapimentel@gmail.com>
2022-01-29 19:36:01 -0300

    requisito 7

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
+import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
-// import Title from './components/Title';
 
 // Tive problemas pra executar o npm start e achei a solução nesse link do stackoverflow, por isso, tive que alterar o package.json.
 // https://stackoverflow.com/questions/69665222/node-js-17-0-1-gatsby-error-digital-envelope-routinesunsupported-err-os
@@ -11,6 +11,7 @@ class App extends React.Component {
       <main>
         <Header />
         <SolarSystem />
+        <Missions />
       </main>
     );
   }
